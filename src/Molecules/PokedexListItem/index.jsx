594036f2f7fa6391cbf24dcad2916223db587e47
John Mello <johncharlesmello@gmail.com>
2022-07-05 10:33:37 -0300

    create pokedex list item

---
+++ src/Molecules/PokedexListItem/index.jsx
@@ -0,0 +1,21 @@
+import './styles.scss'
+const PokedexListItem = () => {
+  return (
+    <li className="listItem">
+      <header className="listItem__header">
+        <span className="listItem__pokemonNumber">#001</span>
+        <h2 className="listItem__pokemonName">Bubassaur</h2>
+      </header>
+      <div className="listItem__InfoContainer">
+        <div className="listItem__pokemonInfoContainer">
+          <span className="listItem__pokemonType">Grass</span>
+          <span className="listItem__pokemonType">Poison</span>
+        </div>
+        <div className="listItem__pokemonImageContainer">
+          <img className='listItem__pokemonImage' src="https://cdn.traction.one/pokedex/pokemon/1.png" alt="" />
+        </div>
+      </div>
+    </li>
+  )
+}
+export default PokedexListItem
