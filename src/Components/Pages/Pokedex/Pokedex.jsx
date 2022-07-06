bb94b7729a46f53cf82df153d4ee59824e82c53b
John Mello <johncharlesmello@gmail.com>
2022-07-06 13:48:13 -0300

    change folder location

--- src/Components/Pages/Pokedex/Pokedex.jsx
+++ src/Components/Pages/Pokedex/Pokedex.jsx
@@ -1,9 +1,14 @@
-import PokedexListItem from '@components/Molecules/listTest'
+import { useEffect } from 'react'
+
+import PokedexListItem from '@components/Molecules/PokemonCard'
 import Navbar from '@components/Organisms/Navbar'
 import Popup from '@components/Organisms/Popup'
 import './styles.scss'
+import useFetch from '@hooks/usePokemonFetch'
 
 const Pokedex = () => {
+  const { pokemonData } = useFetch()
+
   return (
     <div className='pokedexPage'>
       <div className="pokedexPage__container">
@@ -11,15 +16,6 @@ const Pokedex = () => {
         <div className="pokedex">
           <h2 className='pokedex__title'>Pokedex</h2>
           <ul className="pokedex__list">
-            <PokedexListItem />
-            <PokedexListItem />
-            <PokedexListItem />
-            <PokedexListItem />
-            <PokedexListItem />
-            <PokedexListItem />
-            <PokedexListItem />
-            <PokedexListItem />
-            <PokedexListItem />
             <PokedexListItem />
             <Popup />
           </ul>
