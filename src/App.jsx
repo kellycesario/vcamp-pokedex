c3b68bd2813c47d426eb13c25193de09859d5106
kellycesario <kellycesario@gmail.com>
2022-07-12 16:21:24 -0300

    Updating the homepage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Link } from 'react-router-dom';
-
+import Homepage from '@components/Organisms/Homescreen';
 import Pokedex from '@components/Pages/Pokedex'
 
 import './App.scss'
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='App'>
       <Routes>
-        <Route path='/' element={<h2>Home</h2>}></Route>
+        <Route path='/homepage' element={<Homepage />}></Route>
         <Route path='/pokedex' element={<Pokedex />}></Route>
       </Routes>
     </div>
