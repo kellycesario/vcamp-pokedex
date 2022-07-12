103f96935b54731a884464b5f8f318dcc8bafcf7
kellycesario <kellycesario@gmail.com>
2022-07-12 15:00:00 -0300

    Creating the 'ChosenPokemon' organism

--- src/Components/Molecules/PokeCard/index.jsx
+++ src/Components/Molecules/PokeCard/index.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, BrowserRouter, Link } from "react-router-dom"
+import { Route, BrowserRouter, Link, useState } from "react-router-dom"
 
 import style from './style.scss'
 
---
+++ src/Components/Organisms/ChosenPokemon/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Route, BrowserRouter, Link, useState } from "react-router-dom"
+
+import PokeCard from '@components/Molecules/PokeCard'
+
+const ChosenPokemon = () => {
+    return (
+        <>
+            <PokeCard />
+        </>
+    )  
+}
+
+export default ChosenPokemon ;
