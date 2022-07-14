38636767e0d15d0e3f728f39dab816e7f1d54b0a
John Mello <johncharlesmello@gmail.com>
2022-07-14 15:09:12 -0300

    add capitalize to pokemon name - add Link

--- src/Components/Molecules/PokemonCard/index.jsx
+++ src/Components/Molecules/PokemonCard/index.jsx
@@ -6,22 +6,22 @@ import { capitalize, formatId } from './../../../utils/formatData'
 const PokemonCard = ({ name, types, id, sprites }) => {
 
   return (
-    <Link to={`/pokedex/${name}`} className="pokemonCard">
-    <li className={`pokemonCard pokemonCard--${types[0].type.name}`}>
+    <Link to={`/pokedex/${name}`} className="pokemonCard__routerLink">
+      <li className={`pokemonCard pokemonCard--${types[0].type.name}`}>
 
-      <div className="pokemonCard__pokemonInfoLeft">
-        <h2 className="pokemonCard__pokemonName">{capitalize(name)}</h2>
-        {types.map(item => {
-          return (
-            <span key={item.slot} className="pokemonCard__pokemonType">{item.type.name}</span>
-          )
-        })}
-      </div>
-      <div className="pokemonCard__pokemonInfoRight">
-        <span className="pokemonCard__pokemonNumber">{formatId(id)}</span>
-        <img className='pokemonCard__pokemonImage' src={sprites.other['official-artwork']['front_default']} alt="" />
-      </div>
-    </li>
+        <div className="pokemonCard__pokemonInfoLeft">
+          <h2 className="pokemonCard__pokemonName">{capitalize(name)}</h2>
+          {types.map(item => {
+            return (
+              <span key={item.slot} className="pokemonCard__pokemonType">{capitalize(item.type.name)}</span>
+            )
+          })}
+        </div>
+        <div className="pokemonCard__pokemonInfoRight">
+          <span className="pokemonCard__pokemonNumber">{formatId(id)}</span>
+          <img className='pokemonCard__pokemonImage' src={sprites.other['official-artwork']['front_default']} alt="" />
+        </div>
+      </li>
     </Link>
   )
 }
