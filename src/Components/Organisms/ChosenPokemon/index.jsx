2b38f35410f62497701ff41bf2b22948afa62ffa
John Mello <johncharlesmello@gmail.com>
2022-07-14 15:10:21 -0300

    remove unused imports

--- src/Components/Organisms/ChosenPokemon/index.jsx
+++ src/Components/Organisms/ChosenPokemon/index.jsx
@@ -1,14 +1,6 @@
 import React from "react"
-import { Route, BrowserRouter, Link } from "react-router-dom"
-
 import ChosenPokeCard from '@components/Molecules/ChosenPokeCard'
 
-const ChosenPokemon = () => {
-    return (
-        <>
-            <ChosenPokeCard />
-        </>
-    )  
-}
+const ChosenPokemon = () => <ChosenPokeCard />
 
-export default ChosenPokemon ;
+export default ChosenPokemon;
