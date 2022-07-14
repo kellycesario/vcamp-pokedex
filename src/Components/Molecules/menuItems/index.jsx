c386c5de0a694a78fb4bc41abfd97805cdffcf42
John Mello <johncharlesmello@gmail.com>
2022-07-14 15:08:00 -0300

    add navlinks

--- src/Components/Molecules/menuItems/index.jsx
+++ src/Components/Molecules/menuItems/index.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Routes, Route, Router } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import About from '@components/atoms/menuAbout'
 import BaseStats from '@components/atoms/menuBaseStats'
@@ -7,21 +7,27 @@ import Evolution from '@components/atoms/menuEvolution'
 import Moves from '@components/atoms/menuMoves'
 
 const MenuRoutes = () => {
-    return(
+    return (
         <>
             <ul className="pokeCard__list">
-                <li className="pokeCard__item">About</li>
-                <li className="pokeCard__item">Base Stats</li>
-                <li className="pokeCard__item">Evolution</li>
-                <li className="pokeCard__item">Moves</li>
+                <li className="pokeCard__item">
+                    <NavLink to='about'>
+                        About
+                    </NavLink>
+                </li>
+                <li className="pokeCard__item"
+                ><NavLink to='about'>Base Stats </NavLink>
+                </li>
+                <li className="pokeCard__item">
+                    <NavLink to='about'>Evolution </NavLink>
+                </li>
+                <li className="pokeCard__item">
+                    <NavLink to='about'>Moves </NavLink>
+                </li>
             </ul>
 
-        <Routes>
-            <Route element={ <About /> } path="/about" />
-            <Route element={ <BaseStats /> } path="/basestats" />
-            <Route element={ <Evolution /> } path="/evolution" />
-            <Route element={ <Moves /> } path="/moves" />
-        </Routes>
+
+
         </>
     );
 }
