472b044b786867e9d3c4176560bc89b39df55492
John Mello <johncharlesmello@gmail.com>
2022-07-14 15:10:43 -0300

    bring in context

--- src/Components/Pages/Pokedex/index.jsx
+++ src/Components/Pages/Pokedex/index.jsx
@@ -1,30 +1,37 @@
-import PokemonCard from '@components/Molecules/PokemonCard'
-import Navbar from '@components/Organisms/Navbar'
-import Popup from '@components/Organisms/Popup'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { useContext } from 'react';
+import { Context } from "@context/context";
+import { Outlet } from 'react-router-dom'
 
-import './styles.scss'
+import PokemonCard from '@components/Molecules/PokemonCard';
+import Navbar from '@components/Organisms/Navbar';
+
+import Popup from '@components/Organisms/Popup';
+
+import './styles.scss';
 
 const Pokedex = () => {
-   return (
-    <div className='pokedexPage'>
-      <div className="pokedexPage__container">
-        <Navbar />
-        <div className="pokedex">
-          <h2 className='pokedex__title'>Pokedex</h2>
-          <ul className="pokedex__list">
-            {pokemonList.map(pokemon => {
-              return (
-                <PokemonCard name={pokemon.name} id={pokemon.id} types={pokemon.types} sprites={pokemon.sprites} key={pokemon.id} />
-              )
-            })}
-            <Popup />
-          </ul>
-        </div>
+  const PokemonCtx = useContext(Context)
+  return (
+    <>
+      <div className='pokedexPage'>
+        <div className="pokedexPage__container">
+          <Navbar />
+          <div className="pokedex">
+            <h2 className='pokedex__title'>Pokedex</h2>
+            <ul className="pokedex__list">
+              {PokemonCtx.pokemonList.map(pokemon => {
+                return (
+                  <PokemonCard name={pokemon.name} id={pokemon.id} types={pokemon.types} sprites={pokemon.sprites} key={pokemon.id} />
+                )
+              })}
+              <Popup />
+            </ul>
+          </div>
 
+        </div>
       </div>
-    </div>
-  )
-}
-export default Pokedex
+      <Outlet />
+    </>
+  );
+};
+export default Pokedex;
