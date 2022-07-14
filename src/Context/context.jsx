7b9a2ca2eb3ee49306e001ff63fba08feb8d6e43
John Mello <johncharlesmello@gmail.com>
2022-07-14 15:11:11 -0300

    rewrite initial context valu

--- src/Context/context.jsx
+++ src/Context/context.jsx
@@ -1,59 +1,66 @@
 import React, { createContext, useState, useEffect } from "react";
-import {fetchPokemon} from '@services/fetchPokemonData'
+import { fetchPokemon } from '@services/fetchPokemonData'
 import { fetchPokemonData } from "../services/fetchPokemonData";
 
-const initialContextValue = 
-    {pokemonList: [],
-      pokemon: {},
-      pokemonSearch: '',
-        isLoading: true,
-        handleChange: () => {},
-        handleSubmit: () => {}, 
-        setPokemonSearch: () => {}
-    }
+const initialContextValue =
+{
+  pokemonList: [],
+  pokemon: {},
+  pokemonSearch: '',
+  isLoading: true,
+  handleChange: () => { },
+  handleSubmit: () => { },
+  setPokemonSearch: () => { }
+}
 
 export const Context = createContext(initialContextValue);
 
-export const ContextProvider = ({children}) => {
-    const [pokemonList, setPokemonList] = useState([])
-    const [pokemon, setPokemon] = useState({})
-    const [isLoading, setIsLoading] = useState(true)
-    const [pokemonSearch, setPokemonSearch] = useState('')
+export const ContextProvider = ({ children }) => {
+  const [pokemonList, setPokemonList] = useState([])
+  const [pokemon, setPokemon] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+  const [pokemonSearch, setPokemonSearch] = useState('')
 
-    const getPokemonList = async () => {
-      const res = await fetchPokemonData()
-  
-      const { results } = res
-  
-      results.forEach(async (pokemon) => {
-        const res = await fetch(pokemon.url)
-        const data = await res.json()
-        setPokemonList(prevList => [...prevList, data])
-      })
-    }
-  
-    const getPokemon = async (name) => {
-          const pokemonData = await fetchPokemon(name)
-          setPokemon(pokemonData)
-          setIsLoading(false)
-          }
+  const getPokemonList = async () => {
+    const res = await fetchPokemonData()
 
-    const handleSubmit = (ev, pokemonName) => {
-        ev.preventDefault()
-        getPokemon(pokemonName.toLowerCase())
-      }
-      
-      const handleChange = (ev) => {
-        setPokemonSearch(ev.target.value) 
-      }
-      
-      useEffect(() => {
-        getPokemonList()
-      }, [])
-      
-    return (
-        <Context.Provider value={{pokemonList, pokemon, isLoading, handleChange, handleSubmit, setPokemonSearch, pokemonSearch}}>
-            {children}
-        </Context.Provider>
-    )
+    const { results } = res
+
+    results.forEach(async (pokemon) => {
+      const res = await fetch(pokemon.url)
+      const data = await res.json()
+      setPokemonList(prevList => [...prevList, data])
+    })
+  }
+
+  const getPokemon = async (name) => {
+    const pokemonData = await fetchPokemon(name)
+    setPokemon(pokemonData)
+    setIsLoading(false)
+  }
+
+  const handleSubmit = (ev, pokemonName) => {
+    ev.preventDefault()
+    getPokemon(pokemonName.toLowerCase())
+  }
+
+  const handleChange = (ev) => {
+    setPokemonSearch(ev.target.value)
+  }
+
+  useEffect(() => {
+    getPokemonList()
+  }, [])
+
+  // Effect que busca um pokemon quando o valor do pokemon altera
+
+  useEffect(() => {
+    getPokemon(pokemonSearch)
+  }, [pokemonSearch])
+
+  return (
+    <Context.Provider value={{ pokemonList, pokemon, isLoading, handleChange, handleSubmit, setPokemonSearch, pokemonSearch }}>
+      {children}
+    </Context.Provider>
+  )
 }
