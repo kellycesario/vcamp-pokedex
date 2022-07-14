38df9192a1ddfb57eddd9fca8e8ca5e10ae76105
John Mello <johncharlesmello@gmail.com>
2022-07-14 17:27:06 -0300

    bring i pokemonId and pokemon list to filter

--- src/Components/Atoms/menuAbout/index.jsx
+++ src/Components/Atoms/menuAbout/index.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import './style.scss';
 import female from '@assets/icons/female.svg'
 import male from '@assets/icons/male.svg'
+import { useParams } from 'react-router-dom'
+import { useContext } from 'react';
+import { Context } from '@context/context';
+import './style.scss';
+
 
 const About = () => {
-    return(
+    const { pokemonList } = useContext(Context)
+    const { pokemonId } = useParams()
+
+    return (
         <>
-            <p className="pokeCard__aboutText">Bulbasaur can be seen napping in bright sunlight. 
-                There is a seed on its back. By soaking up the sun's rays, 
+            <p className="pokeCard__aboutText">Bulbasaur can be seen napping in bright sunlight.
+                There is a seed on its back. By soaking up the sun's rays,
                 the seed grows progressively larger. </p>
 
             <div className="pokeCard__aboutCharacteristics">
@@ -20,15 +28,15 @@ const About = () => {
 
             <div className="pokeCard__aboutBreeding">
                 <p className="pokeCard__aboutBreedingTitle">Breeding</p>
-                    <span className="pokeCard__aboutGender">Gender</span>
-                        <p className="pokeCard__aboutMale"> <img src={male} /> 87.5% </p>
-                        <p className="pokeCard__aboutFemale"> <img src={female} /> 12.5% </p>
-                    
-                    <span className="pokeCard__aboutEggGroups">Egg Groups</span>
-                        <p className="pokeCard__aboutGroup">Monster</p>
-                    
-                    <span className="pokeCard__aboutEggCycles">Egg Cycle</span>
-                        <p className="pokeCard__aboutCycle">Grass</p>
+                <span className="pokeCard__aboutGender">Gender</span>
+                <p className="pokeCard__aboutMale"> <img src={male} /> 87.5% </p>
+                <p className="pokeCard__aboutFemale"> <img src={female} /> 12.5% </p>
+
+                <span className="pokeCard__aboutEggGroups">Egg Groups</span>
+                <p className="pokeCard__aboutGroup">Monster</p>
+
+                <span className="pokeCard__aboutEggCycles">Egg Cycle</span>
+                <p className="pokeCard__aboutCycle">Grass</p>
             </div>
         </>
     );
