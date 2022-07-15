488bd6d2f75f1d46db0f6d36126336ab9e2e12b3
John Mello <johncharlesmello@gmail.com>
2022-07-15 13:09:38 -0300

    get random pokemon description, weight, height

--- src/Components/Atoms/menuAbout/index.jsx
+++ src/Components/Atoms/menuAbout/index.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
-import './style.scss';
-import female from '@assets/icons/female.svg'
-import male from '@assets/icons/male.svg'
-import { useParams } from 'react-router-dom'
+
 import { useContext } from 'react';
 import { Context } from '@context/context';
+
+import { formatMeasure, formatWeightToPounds, formatHeightToInches } from '@utils/formatData';
+
+import female from '@assets/icons/female.svg'
+import male from '@assets/icons/male.svg'
+
 import './style.scss';
 
-
 const About = () => {
-    const { pokemon } = useContext(Context)
+    const {
+        pokemon,
+        pokemonDescription,
+        pokemonGender
+    } = useContext(Context)
 
+    const { height, weight } = pokemon
+
+    const englishDescription = pokemonDescription['flavor_text_entries'].filter(text => text.language.name === 'en')
+    const randomDescription = englishDescription[Math.floor(Math.random() * englishDescription.length)]['flavor_text']
+
+    console.log(pokemonGender)
     return (
         <>
-            <p className="pokeCard__aboutText">Bulbasaur can be seen napping in bright sunlight.
-                There is a seed on its back. By soaking up the sun's rays,
-                the seed grows progressively larger. </p>
+            <p className="pokeCard__aboutText">{randomDescription}</p>
 
             <div className="pokeCard__aboutCharacteristics">
                 <span className="pokeCard__aboutHeight">Height</span>
-                <p className="pokeCard__aboutHeightData">2’3.6” (0.70 cm)</p>
+                <p className="pokeCard__aboutHeightData">{`${formatHeightToInches(formatMeasure(height))} inches ${formatMeasure(height)} cm`}</p>
 
                 <span className="pokeCard__aboutWeight">Weight</span>
-                <p className="pokeCard__aboutWeightData">15.2 lbs (6.9 kg)</p>
+                <p className="pokeCard__aboutWeightData">{`${formatWeightToPounds(formatMeasure(weight))} lbs ${formatMeasure(weight)} Kg`}</p>
             </div>
 
             <div className="pokeCard__aboutBreeding">
