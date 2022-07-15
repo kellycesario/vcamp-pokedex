34dc4f5958bf94cfe59ccd23f88bdf9bff55142d
kellycesario <kellycesario@gmail.com>
2022-07-15 10:01:21 -0300

    Adjusting the menu/details and adding watch sassinto more folders

--- src/Components/Molecules/menuItems/index.jsx
+++ src/Components/Molecules/menuItems/index.jsx
@@ -1,23 +1,31 @@
 import React from "react"
 import { NavLink } from "react-router-dom";
 
+import './style.scss'
+
 const MenuRoutes = () => {
     return (
         <>
             <ul className="pokeCard__list">
                 <li className="pokeCard__item">
-                    <NavLink to='about'>
+                    <NavLink to='about' className="pokeCard__navigation">
                         About
                     </NavLink>
                 </li>
-                <li className="pokeCard__item"
-                ><NavLink to='basestats'>Base Stats </NavLink>
+                <li className="pokeCard__item">
+                    <NavLink to='basestats'className="pokeCard__navigation">
+                        Base Stats 
+                    </NavLink>
                 </li>
                 <li className="pokeCard__item">
-                    <NavLink to='evolution'>Evolution </NavLink>
+                    <NavLink to='evolution' className="pokeCard__navigation">
+                        Evolution 
+                    </NavLink>
                 </li>
                 <li className="pokeCard__item">
-                    <NavLink to='moves'>Moves </NavLink>
+                    <NavLink to='moves' className="pokeCard__navigation">
+                        Moves 
+                    </NavLink>
                 </li>
             </ul>
 
