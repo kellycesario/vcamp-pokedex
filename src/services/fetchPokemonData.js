91d44509e026b5050efb5186bd18169099f64ee6
John Mello <johncharlesmello@gmail.com>
2022-07-15 13:08:37 -0300

    add fetchDescription and fetchGender

--- src/services/fetchPokemonData.js
+++ src/services/fetchPokemonData.js
@@ -21,3 +21,27 @@ export const fetchPokemon = async (pokemon) => {
   }
 }
 
+export const fetchDescription = async (pokemon) => {
+  if (!pokemon) return
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`)
+    if (!res.ok) return
+    const data = await res.json()
+    return data
+  } catch (error) {
+    return error
+  }
+}
+
+export const fetchPokemonGender = async (pokemon) => {
+  if (!pokemon) return
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/gender/${pokemon}`)
+    if (!res.ok) return
+    const data = await res.json()
+    return data
+  } catch (error) {
+    return error
+  }
+}
+
